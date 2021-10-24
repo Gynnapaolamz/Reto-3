32c24340441d3aa9c533e10f4dfd4bd14cdd5693
CarlosMCL <cmconrado@unicesar.edu.co>
2021-10-23 22:30:00 -0500

    act

--- reto3/src/componentes/quienesSomos/QuienesSomos.jsx
+++ reto3/src/componentes/quienesSomos/QuienesSomos.jsx
@@ -5,17 +5,9 @@ import Testimonials from '../../componentes/carrusel/Testimonials';
 import Footer from '../../componentes/footer/Footer';
 import Historia from '../../componentes/historia/Historia';
 import Empleados from '../../componentes/empleados/Empleados';
-import personal_1 from '../../media/personal1@2x.png';
-import personal_2 from '../../media/personal2@2x.png';
-import personal_3 from '../../media/personal3@2x.png';
-import personal_4 from '../../media/personal4@2x.png';
-import personal_5 from '../../media/personal5@2x.png';
-import personal_6 from '../../media/personal6@2x.png';
-
-
 
 const QuienesSomos = () => {   
-    var personal;
+    
         return (
             <Fragment>
                 <Encabezado/>
