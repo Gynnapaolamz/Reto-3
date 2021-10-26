9002618e285c8a483fc7fa7234c5db4fe6ede610
Gynna Paola Mogollon Zabala <86632783+Gynnapaolamz@users.noreply.github.com>
2021-10-26 17:57:29 -0500

    nuevo

--- reto3/src/componentes/Carrousel.js
+++ reto3/src/componentes/Carrousel.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
+import "../../src/css/bootstrap.min.css";
+import Imagen1 from "../../src/media/img1";
+import Imagen2 from "../../src/media/img2";
+import Imagen3 from "../../src/media/img3";
+import Imagen4 from "../../src/media/img4";
+import Imagen5 from "../../src/media/img5";
 
-const Carrousel = (props) => {
+const Carrousel = () => {
  
     return (
       <>
-        <div className="carrousel">
-          <img src={props.image} alt="Imagen de platos" />
-        </div>
+      <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={Imagen1}/>
+              </div>
+              <div className="carousel-item">
+                <img src={Imagen2}/>
+              </div>
+              <div className="carousel-item">
+                <img src={Imagen3}/>
+              </div>
+              <div className="carousel-item">
+                <img src={Imagen4}/>
+              </div>
+              <div className="carousel-item">
+                <img src={Imagen5}/>
+              </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
       </>
     );
   }
--- reto3/src/componentes/inicio/Inicio.jsx
+++ reto3/src/componentes/inicio/Inicio.jsx
@@ -21,35 +21,8 @@ const Inicio = () => {
           <Fragment>
           <Encabezado/>
        
-          <div id="carouselExampleFade" className="carousel slide carousel-fade justify-content-center " data-bs-ride="carousel">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <Carrousel image={Imagen1}/>
-              </div>
-              <div className="carousel-item">
-                <Carrousel image={Imagen2}/>
-              </div>
-              <div className="carousel-item">
-                <Carrousel image={Imagen3}/>
-              </div>
-              <div className="carousel-item">
-                <Carrousel image={Imagen4}/>
-              </div>
-              <div className="carousel-item">
-                <Carrousel image={Imagen5}/>
-              </div>
-              </div>
-              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-              </button>
-            </div>
-
-            
+          <Carrousel/> 
+                     
         <Recomendados/>
          
           <Eventos/> 
