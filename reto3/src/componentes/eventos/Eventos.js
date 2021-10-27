299bc612829f026940b940f26487fd7050d3cf1b
Gynna Paola Mogollon Zabala <86632783+Gynnapaolamz@users.noreply.github.com>
2021-10-27 18:01:48 -0500

    nuevo

--- reto3/src/componentes/eventos/Eventos.js
+++ reto3/src/componentes/eventos/Eventos.js
@@ -15,114 +15,53 @@ import {BrowserRouter as Router,
 
 const Eventos = () => {
     
-        return (
-            <>
-            <div className="container">
-              
-            <div className="titulo even-style-imagen2" id="eventos" >
-                 <h1> <strong>Organizamos tu evento</strong></h1>
-              </div>
-              
-              <div className="container-card">
+     return (
+      <>
+        <div className="container">
+         <div className="titulo even-style-imagen2" id="eventos" >
+          <h1>Organizamos tu evento</h1>
+          </div>
+           <div className="container-card">
               <div className="card" >
-  <img src={Logo} className="card-img-top" alt="Logo1"/>
+              <div className="img">
+       <img src={Logo} className="card-img-top" alt="Logo1"/> </div>
   <div className="card-body">
-    <h2>Fiestas Tematicas</h2>
+    <h3>Fiestas Tematicas</h3>
     <p className="card-text">
       Contamos con las instalaciones para organizar tus eventos especiales, con la tematica e idea que desees.</p>
-    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
   </div>
+  <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
 </div>
 
 <div className="card" >
-  <img src={Logo2} className="card-img-top" alt="Logo2"/>
+  <div className="img">
+  <img src={Logo2} className="card-img-top" alt="Logo2"/> </div>
   <div className="card-body">
-  <h2>Matrimonios</h2>
+  <h3>Matrimonios</h3>
     <p className="card-text">Dejanos ser parte de este momento tan especial, organizando cada uno de los detalles para que sea el dia que soñaste siempre.</p>
-    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
   </div>
+    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
 </div>
 <div className="card" >
-  <img src={Logo3} className="card-img-top" alt="Logo3"/>
+  <div className="img">
+  <img src={Logo3} className="card-img-top" alt="Logo3"/> </div>
   <div className="card-body">
-    <h2>Cenas Empresariales</h2>
+    <h3>Cenas Empresariales</h3>
     <p className="card-text">Aqui puedes organizar tus eventos empresariales, comerciales y personales que requieras, con la mejor carta e instalaciones de la ciudad.</p>
-    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
-  </div>
+      </div>
+      <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
 </div>
 <div className="card" >
-  <img src={Logo4} className="card-img-top" alt="Logo4"/>
+  <div className="img">
+  <img src={Logo4} className="card-img-top" alt="Logo4"/> </div>
   <div class="card-body">
-  <h2>Cumpleaños</h2>
+  <h3>Cumpleaños</h3>
     <p className="card-text">Sabemos que es un día importante, puedes disfrutar de una cena especial y luego complementarla con unas copas en compañia de tus seres queridos.</p>
-    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
+      </div>
+      <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
+</div>
+</div>
   </div>
-</div>
-</div>
-            <div className="container-2 even-style-imagen">
-              <div className="container even-style-imagen2" id="eventos" >
-                
-                    <h1 className="titulo"> <strong>Organizamos tu evento</strong></h1>
-                  </div>
-                </div>
-                
-
-                <div className="row">
-                  <div className="col-md-3">
-                    <img className="img-evento" src={Logo}/>
-                  </div>
-                  <div className="col-md-3">
-                    <img className="img-evento" src={Logo2}/>
-                  </div>
-                  <div className="col-md-3">
-                    <img className="img-evento" src={Logo3}/>
-                  </div>
-                  <div className="col-md-3">
-                    <img className="img-evento" src={Logo4}/>
-                  </div>
-                </div>  
-                <div className="row">
-                  <div className="col-md-3">
-                    <h5 className="h5">Fiestas Tematicas</h5>
-                    <p className="evento-text">
-                      Contamos con las instalaciones para organizar tus eventos especiales, con la tematica e idea que desees.
-                    </p>
-                  </div>
-                  <div className="col-md-3">
-                    <h5 className="h5">Matrimonios</h5>
-                    <p className="evento-text">
-                      Dejanos ser parte de este momento tan especial, organizando cada uno de los detalles para que sea el dia que soñaste siempre.
-                    </p>
-                  </div>
-                  <div className="col-md-3">
-                    <h5 className="h5">Cenas empresariales</h5>
-                    <p className="evento-text">
-                      Aqui puedes organizar tus eventos empresariales, comerciales y personales que requieras, con la mejor carta e instalaciones de la ciudad.
-                    </p>
-                  </div>
-                  <div className="col-md-3">
-                    <h5 className="h5">Cumpleaños</h5>
-                    <p className="evento-text">
-                      Sabemos que es un día importante, puedes disfrutar de una cena especial y luego complementarla con unas copas en compañia de tus seres queridos.
-                    </p>
-                  </div>
-        
-                </div>
-                <div className="row">
-                  <div className="col-md-3">
-                    <Link to ="/servicios"><img className="boton"src={Boton}/></Link>
-                  </div>
-                  <div className="col-md-3">
-                    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
-                  </div>
-                  <div className="col-md-3">
-                    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
-                  </div>
-                  <div className="col-md-3">
-                    <Link to ="/servicios"><img className="boton" src={Boton}/></Link>
-                  </div>
-                </div> 
-              </div>
           
             </> 
         );
